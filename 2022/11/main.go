f48f483c7a29b5ce9d8555bdb6882190c9e7a1bd
Ondrej Balaz <blami@blami.net>
2022-12-11 04:55:27 +0900

    Add solution of day 11 of 2022

--- 2022/11/main.go
+++ 2022/11/main.go
@@ -1,3 +1,6 @@
+// Read input from stdin and run monkey business while tracking how many items
+// each monkey inspected.
+
 package main
 
 import (
@@ -8,13 +11,14 @@ import (
 	"strings"
 )
 
+// A monkey.
 type Monkey struct {
-	Items []int
-	Op string // needs to be eval()'d
-	Test int // seems to be always "divisble by"
-	True int // is always throw to N
-	False int // is always throw to N
-	Insp int // number of inspected items
+	Items []int  // Currently held item worry levels.
+	Op    string // Operation applied to worry level while inspecting item.
+	Test  int    // Divisor in test.
+	True  int    // Target if test passes.
+	False int    // Target if test fails.
+	Insp  int    // Number of inspected items.
 }
 
 // Evaluate given operation given the old value and return new value.
@@ -22,13 +26,21 @@ func eval(op string, old int) int {
 	a, b := 0, 0
 	var err error
 	l := strings.Split(op, " ")
-	if l[0] == "old" { a = old } else { 
-		a, err = strconv.Atoi(l[0]) 
-		if err != nil { log.Fatal("wrong operand a: ", l) }
+	if l[0] == "old" {
+		a = old
+	} else {
+		a, err = strconv.Atoi(l[0])
+		if err != nil {
+			log.Fatal("wrong operand a: ", l)
+		}
 	}
-	if l[2] == "old" { b = old } else {
+	if l[2] == "old" {
+		b = old
+	} else {
 		b, err = strconv.Atoi(l[2])
-		if err != nil { log.Fatal("wrong operand b: ", l) }
+		if err != nil {
+			log.Fatal("wrong operand b: ", l)
+		}
 	}
 	switch l[1] {
 	case "+":
@@ -45,39 +57,34 @@ func eval(op string, old int) int {
 	return -1
 }
 
-// Run monkey business for given number of rounds and worry relieve factor.
-func business(monkeys []Monkey, rounds int, relieve int) []Monkey {
+// Run monkey business for given number of rounds using given worry level
+// management function.
+func business(monkeys []Monkey, rounds int, w func(int) int) []Monkey {
 	for rounds > 0 {
-		for n, m := range(monkeys) {
-			//log.Print("Monkey ", n)
-			for _, oold := range(m.Items) {
-				//log.Print(" inspecting ", oold)
-				nnew := eval(m.Op, oold)
-				//log.Print(" changed ", nnew)
-				nnew = nnew / relieve // relieve factor
-				//log.Print(" relieved ", nnew)
-				if nnew % m.Test == 0 {
-					//log.Print(" throwing to ",nnew,  m.True)
-					monkeys[m.True].Items = append(monkeys[m.True].Items, nnew)
+		for i, m := range monkeys {
+			for _, o := range m.Items {
+				n := eval(m.Op, o)
+				n = w(n) // change worry level
+				if n%m.Test == 0 {
+					monkeys[m.True].Items = append(monkeys[m.True].Items, n)
 				} else {
-					//log.Print(" throwing to ", nnew, m.False)
-					monkeys[m.False].Items = append(monkeys[m.False].Items, nnew)
+					monkeys[m.False].Items = append(monkeys[m.False].Items, n)
 				}
-				// count inspected items; DO NOT use m. as that is copy
-				monkeys[n].Insp++
+				monkeys[i].Insp++
 			}
-			// empty items; DO NOT use m. as that is copy
-			monkeys[n].Items = nil
+			// empty items; DO NOT use m.Items = nil as that is a copy
+			monkeys[i].Items = nil
 		}
 		rounds--
 	}
 	return monkeys
 }
 
-// Return product of two most active monkeys inspection counts.
+// Find two most active monkeys and return product of number of items they
+// inspected.
 func most(out []Monkey) int {
 	mam := [2]int{}
-	for _, m := range(out) {
+	for _, m := range out {
 		if m.Insp > mam[0] {
 			mam[1] = mam[0]
 			mam[0] = m.Insp
@@ -88,56 +95,46 @@ func most(out []Monkey) int {
 	return mam[0] * mam[1]
 }
 
-
 func main() {
 
-	// NOTE: thought of using map but monkeys go in sequence...
 	monkeys := []Monkey{}
+	lcm := 1 // least common multiple of test divisors
 
-	// Process the input
 	s := bufio.NewScanner(os.Stdin)
 	// BUG: this is sloppy in case line would be so long it does not fit
 	// internal buffer but for input.txt is fine
 	for s.Scan() {
 		l := s.Text()
 
-		// NOTE: assuming good input only
+		// NOTE: assuming good input only; not particularly proud about this
 		if len(l) > 1 && l[0] != ' ' { // top level Monkey section
 			n, _ := strconv.Atoi(strings.Split(l[:len(l)-1], " ")[1])
 			m := Monkey{}
-			// read Monkey block
 			for {
 				s.Scan()
 				l := strings.Trim(s.Text(), " ")
 				if l == "" {
 					break
 				}
-				// NOTE: rather handle Atoi, etc. errors here as we don't know
-				// the full syntax of input from examples...
 				switch {
 				case l[:2] == "St": // Starting items
-					for _, v := range(strings.Split(strings.Trim(strings.Split(l, ":")[1], " "), ",")) {
-						item, err := strconv.Atoi(strings.Trim(v, " "))
-						if err != nil {
-							log.Fatal("invalid start item: ", l)
-						}
+					for _, v := range strings.Split(strings.Trim(strings.Split(l, ":")[1], " "), ",") {
+						item, _ := strconv.Atoi(strings.Trim(v, " "))
 						m.Items = append(m.Items, item)
 					}
 				case l[:2] == "Op": // Operation
 					m.Op = strings.Trim(strings.Split(strings.Split(l, ":")[1], "=")[1], " ")
 				case l[:2] == "Te": // Test
 					// Seems to be always "divisble by" so save only the integer
-					var err error
-					m.Test, err = strconv.Atoi(strings.Split(strings.Split(l, ":")[1], " ")[3])
-					if err != nil {
-						log.Fatal("invalid test divsible by: ", l)
-					}
+					m.Test, _ = strconv.Atoi(strings.Split(strings.Split(l, ":")[1], " ")[3])
+					// NOTE: for part 2 worry level numbers easily overflow
+					// int, to manage we calculate least common multiplier of
+					// all divisors and instead of /3 (part 1) we %lcm to keep
+					// worry levels "right" but within int range.
+					lcm *= m.Test
 				case l[:2] == "If": // Conditions
 					ll := strings.Split(l, " ")
-					targ, err := strconv.Atoi(ll[5])
-					if err != nil {
-						log.Fatal("invalid throw target: ", l)
-					}
+					targ, _ := strconv.Atoi(ll[5])
 					switch ll[1] {
 					case "true:":
 						m.True = targ
@@ -148,9 +145,8 @@ func main() {
 					}
 				}
 			}
-			if n != len(monkeys) {
-				log.Fatal("monkey sequence broken")
-			}
+			// works for my input, not sure whether monkeys can come in
+			// different order...
 			monkeys = append(monkeys, m)
 		}
 	}
@@ -158,10 +154,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Run monkey business
-	out1 := business(append([]Monkey{}, monkeys...), 20, 3)
-	//out2 := business(append([]Monkey{}, monkeys...), 10000, 1)
+	// run monkey business
+	// NOTE: copy monkeys slice for part1 and part2
+	out1 := business(append([]Monkey{}, monkeys...), 20, func(w int) int { return w / 3 })
+	out2 := business(append([]Monkey{}, monkeys...), 10000, func(w int) int { return w % lcm })
 
 	log.Print(most(out1))
-	//log.Print(most(out2))
+	log.Print(most(out2))
 }
