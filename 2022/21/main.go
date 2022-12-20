4d5145e77be39c1fb0f7a4e38269ea45924be8ad
Ondrej Balaz <blami@blami.net>
2022-12-21 02:08:49 +0900

    Add solution of day 21 of 2022

--- 2022/21/main.go
+++ 2022/21/main.go
@@ -8,34 +8,62 @@ import (
 	"strings"
 )
 
+// A Monkey.
 type Monkey struct {
 	l, r string // left right monkeys
-	op byte
-	num int
-	done bool
+	op   byte
+	num  float64
 }
 
+// Map of monkeys.
+type Monkeys map[string]Monkey
+
+// Resolve given monkey's number without mutating underlying map.
+func (ms *Monkeys) resolve(n string, guess bool, humn float64) float64 {
+	if guess && n == "humn" {
+		return humn
+	}
+	m := (*ms)[n]
+	if m.op == '#' {
+		return m.num
+	}
+	l := ms.resolve(m.l, guess, humn)
+	r := ms.resolve(m.r, guess, humn)
+	switch m.op {
+	case '+':
+		return l + r
+	case '-':
+		return l - r
+	case '*':
+		return l * r
+	case '/':
+		return l / r
+	default:
+		log.Fatalf("%s: unknown op %c", n, m.op)
+		return 0 //duh
+	}
+}
 
 func main() {
 
-	ms := map[string]Monkey{}
+	ms := Monkeys{}
 
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		l := strings.Split(s.Text(), ":")
 
 		m := Monkey{}
-		nam := l[0] // name
+		nam := l[0]                    // name
 		job := strings.Trim(l[1], " ") // job
-		num, err := strconv.Atoi(job) // number
+		num, err := strconv.Atoi(job)  // number
 		if err != nil {
 			lrop := strings.Split(job, " ")
 			m.l = lrop[0]
 			m.op = lrop[1][0] // +-*/
 			m.r = lrop[2]
 		} else {
-			m.num = num
-			m.done = true
+			m.num = float64(num)
+			m.op = '#' // monkey just tells number
 		}
 
 		ms[nam] = m
@@ -44,34 +72,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// resolve
-	i := 0
-	done := false
-	for !done {
-		done = true
-		for nam := range ms {
-			m := ms[nam]
-			if m.done { continue }
-			if ms[m.l].done && ms[m.r].done {
-				switch ms[nam].op {
-				case '+':
-					m.num = ms[m.l].num + ms[m.r].num
-				case '-':
-					m.num = ms[m.l].num - ms[m.r].num
-				case '*':
-					m.num = ms[m.l].num * ms[m.r].num
-				case '/':
-					m.num = ms[m.l].num / ms[m.r].num
-				default:
-					log.Fatal(nam, ".op: ", m.op)
-				}
-				m.done = true
-				ms[nam] = m
-			}
-			if !ms[nam].done { done = false }
+	root := ms.resolve("root", false, 0)
+
+	// this is to get halving condition right
+	gt := ms.resolve(ms["root"].r, true, 0) > ms.resolve(ms["root"].l, true, 0)
+
+	e := ms.resolve(ms["root"].r, true, 0) // resolve root.l with humn=0; expected value
+	l, h := int(-root*5), int(root*5)      // low, high for binsearch; TOGGLE if needed
+	humn := 0
+	for l < h {
+		humn = (l + h) / 2                                 // shoot in mid
+		a := ms.resolve(ms["root"].l, true, float64(humn)) // resolve root.r with humn=(l+h)/2 as a guess
+		//log.Printf("l=%d h=%d humn=%d e=%f a=%f", l, h, humn, e, a)
+		if a == e {
+			break // humn causes match
 		}
-		i++
+		if (!gt && a < e) || (gt && a > e) {
+			h = humn
+		} else {
+			l = humn
+		} // binary halving
 	}
 
-	log.Print(i, " ", ms, " ", ms["root"].num)
+	log.Print(int(root))
+	log.Print(humn)
 }
